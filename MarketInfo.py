5038b6dd5f861c7bf69932dde07fc728b71084ef
ProcessL34 <liucheng@s-MacBook-Air.local>
2023-06-15 01:00:53 +0800

    add two methods

--- MarketInfo.py
+++ MarketInfo.py
@@ -1,17 +1,33 @@
 import requests
 import pandas as pd
 import plotly.graph_objects as go
-from okx.Trade import TradeAPI
 
 url = 'https://aws.okx.com'
 
-tickers = pd.DataFrame((requests.get(url + '/api/v5/market/tickers?instType=SWAP').json())['data'])
-tickers = tickers.drop('instType', axis=1)
-print(len(tickers))
-# print(tickers.tail().T)
 
-ticker = requests.get(url + '/api/v5/market/ticker?instId=BTC-USD-SWAP').json()
-# print(ticker)
+def get_sorted_tickers():
+    tickers = pd.DataFrame((requests.get(url + '/api/v5/market/tickers?instType=SWAP').json())['data'])
+    print(len(tickers))
+    # print(tickers.tail())
+    tickers['sodUtc8'] = tickers['sodUtc8'].astype(float)
+    tickers['last'] = tickers['last'].astype(float)
+    tickers['growth_rate'] = (tickers['last'] - tickers['sodUtc8']) / tickers['sodUtc8']
+    tickers['growth_rate'] = tickers['growth_rate'].apply(lambda x: '{:.3%}'.format(x))
+    tickers = tickers.sort_values(by=['growth_rate'], ascending=False)
+    print(tickers)
+    return tickers
+
+
+def get_coin_growth(tickers, inst_id):
+    growth_rate = tickers.loc[tickers['instId'] == inst_id, 'growth_rate'].values[0]
+    print('btc growth rate', growth_rate)
+    return growth_rate
+
+
+sorted_tickers = get_sorted_tickers()
+
+btc_growth_rate = get_coin_growth(sorted_tickers, 'BTC-USDT-SWAP')
+
 
 historical = pd.DataFrame((requests.get(url + '/api/v5/market/history-candles?instId=BTC-USDT-SWAP').json())['data'])
 # print(historical)
@@ -36,6 +52,3 @@ fig = go.Figure(data=[go.Candlestick(x=historical.index,
 trades = requests.get(url + '/api/v5/market/trades?instId=BTC-USDT').json()
 var = trades['data']
 # print(var)
-
-
-
