d4a507893d1a9b49e6a0413e80bdd8cfec03a592
ProcessL34 <liucheng@s-MacBook-Air.local>
2023-07-04 00:50:25 +0800

    test interface

--- MarketInfo.py
+++ MarketInfo.py
@@ -1,6 +1,5 @@
 import requests
 import pandas as pd
-import plotly.graph_objects as go
 
 url = 'https://aws.okx.com'
 
@@ -28,27 +27,3 @@ sorted_tickers = get_sorted_tickers()
 
 btc_growth_rate = get_coin_growth(sorted_tickers, 'BTC-USDT-SWAP')
 
-
-historical = pd.DataFrame((requests.get(url + '/api/v5/market/history-candles?instId=BTC-USDT-SWAP').json())['data'])
-# print(historical)
-historical.columns = ["Date", "Open", "High", "Low", "Close", "Vol", "volCcy", "volCcyQuote", "confirm"]
-historical['Date'] = pd.to_datetime(historical['Date'], unit='ms')
-historical.set_index('Date', inplace=True)
-historical.sort_values(by='Date')
-# print(historical)
-historical['20 SMA'] = historical.Close.rolling(20).mean()
-historical.tail()
-
-fig = go.Figure(data=[go.Candlestick(x=historical.index,
-                                     open=historical['Open'],
-                                     high=historical['High'],
-                                     low=historical['Low'],
-                                     close=historical['Close'],
-                                     ),
-                      go.Scatter(x=historical.index, y=historical['20 SMA'], line=dict(color='purple', width=1))])
-
-# fig.show()
-
-trades = requests.get(url + '/api/v5/market/trades?instId=BTC-USDT').json()
-var = trades['data']
-# print(var)
